31baa948384b99101ea8f5fc9abb0c1c52c2b2a6
Ghaff <mudashiruagm@gmail.com>
2019-05-22 09:24:12 +0000

    Created the DepositSlot class

---
+++ src/atm/DepositSlot.java
@@ -0,0 +1,12 @@
+package atm;
+
+//Represents the deposit slot of the ATM
+public class DepositSlot
+{
+	//indicates whether envelop was received (always return true 
+	//because this is only a software simulation of a real deposit slot)
+	public boolean isEnvelopeReceived()
+	{
+		return true; //deposit envelop was received
+	}//end method isEnvelopeReceived
+}//end class DepositSlot
